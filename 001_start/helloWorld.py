7ce985fc445e14beda2283a1ccf263b0b5b79a9d
techking11 <nma1132000@gmail.com>
2025-02-09 23:01:38 +0700

    add start project with hello world

---
+++ 001_start/helloWorld.py
@@ -0,0 +1,20 @@
+# start the project
+# print('hello world')
+
+# x = input("What's your age? ")
+# # print(*Object, sep=" ", end="\n")
+# print("hello, ", sep="????", end="")
+# print(x)
+
+
+# x = input("What's your name? ")
+# print("hello", x)
+
+# print the specific format
+# print(f"hahah")
+
+x = input("What is your education? ")
+x = x.strip().title()
+print("Your education background is " + x)
+# format output line
+print(f"Education {x}")
