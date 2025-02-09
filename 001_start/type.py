c01514d5e6fb3cc49dc570ddc417068fa1c3fde4
techking11 <nma1132000@gmail.com>
2025-02-09 23:09:04 +0700

    add type

---
+++ 001_start/type.py
@@ -0,0 +1,14 @@
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+# work as the string concetination
+# print(x + y)
+
+# x = int(input("Enter the fist count: "))
+# y = int(input("Enter the second count: "))
+# print(x + y)
+
+# float type
+x = float(input("Enter the students count: "))
+y = float(input("Enter the teachers count: "))
+# print(int(x + y), float(x + y))
+print(round(x/y, 5))
